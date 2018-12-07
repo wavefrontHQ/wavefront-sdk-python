0993e95923644b4577b8ced47e7baa9e2c7a65f2
Hao Song <hao@hsong.net>
2018-12-07 15:28:32 -0800

    Remove redundant import in __init__.py

--- wavefront_sdk/common/__init__.py
+++ wavefront_sdk/common/__init__.py
@@ -9,12 +9,6 @@ from __future__ import absolute_import
 
 # flake8: noqa
 
-from wavefront_sdk.common.connection_handler import ConnectionHandler
-from wavefront_sdk.common.utils import AtomicCounter
-from wavefront_sdk.common.utils import chunks
-from wavefront_sdk.common.utils import gzip_compress
-from wavefront_sdk.common.utils import is_blank
-from wavefront_sdk.common.utils import sanitize
 from wavefront_sdk.common.utils import metric_to_line_data
 from wavefront_sdk.common.utils import histogram_to_line_data
 from wavefront_sdk.common.utils import tracing_span_to_line_data
