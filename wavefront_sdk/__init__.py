e473ce8e4b3802610e0a30d1253cf968f23c9e54
Artem Ustinov <artem.ustinov@gmail.com>
2019-04-09 22:51:01 -0700

    Set __version__ Only for Installed Distribution.

--- wavefront_sdk/__init__.py
+++ wavefront_sdk/__init__.py
@@ -6,10 +6,21 @@ spans to Wavefront via proxy or direct ingestion.
 
 @author Hao Song (songhao@vmware.com)
 """
+
 import pkg_resources
 
 from wavefront_sdk.direct import WavefrontDirectClient
 from wavefront_sdk.proxy import WavefrontProxyClient
 
+
 __all__ = ['WavefrontDirectClient', 'WavefrontProxyClient']
-__version__ = pkg_resources.get_distribution('wavefront-sdk-python').version
+
+__version__ = None
+
+try:
+    __version__ = pkg_resources.get_distribution(
+        'wavefront-sdk-python'
+    ).version
+except pkg_resources.DistributionNotFound:
+    # __version__ is only available when distribution is installed.
+    pass
