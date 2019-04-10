86302e632719474ac5bd2b603761afd506704f55
Artem Ustinov <artem.ustinov@gmail.com>
2019-04-09 21:37:13 -0700

    Get __version__ in __init__.py from pkg_resources.

--- wavefront_sdk/__init__.py
+++ wavefront_sdk/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """Wavefront Python SDK.
 
 This library provides support for sending metrics, histograms and opentracing
@@ -7,13 +6,10 @@ spans to Wavefront via proxy or direct ingestion.
 
 @author Hao Song (songhao@vmware.com)
 """
-import pkgutil
+import pkg_resources
 
 from wavefront_sdk.direct import WavefrontDirectClient
 from wavefront_sdk.proxy import WavefrontProxyClient
 
-__version__ = pkgutil.get_distribution(
-    'wavefront-sdk-python').metadata['version']
-
-__all__ = ['WavefrontDirectClient',
-           'WavefrontProxyClient']
+__all__ = ['WavefrontDirectClient', 'WavefrontProxyClient']
+__version__ = pkg_resources.get_distribution('wavefront-sdk-python').version
