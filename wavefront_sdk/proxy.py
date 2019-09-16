372eb170947450a0dfad1952de21351bec2b0887
Hao Song <songhao@vmware.com>
2019-09-16 10:33:45 -0700

    Close metrics registry for proxy reporter

--- wavefront_sdk/proxy.py
+++ wavefront_sdk/proxy.py
@@ -108,6 +108,7 @@ class WavefrontProxyClient(entities.WavefrontMetricSender,
             self._histogram_proxy_connection_handler.close()
         if self._tracing_proxy_connection_handler:
             self._tracing_proxy_connection_handler.close()
+        self._sdk_metrics_registry.close()
 
     def get_failure_count(self):
         """Get Total Failure Count for all connections.
