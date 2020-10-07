fa752eba1231007727de3c79003fcfb5b4ad3de9
Joanna Ko <joannak@vmware.com>
2020-10-07 14:56:18 -0700

    Build Delta Counter

--- wavefront_sdk/common/metrics/deltacounter.py
+++ wavefront_sdk/common/metrics/deltacounter.py
@@ -7,6 +7,4 @@ from wavefront_sdk.common.metrics.counter import WavefrontSdkCounter
 
 
 class WavefrontSdkDeltaCounter(WavefrontSdkCounter):
-    """Wavefront SDK Delta Counter. Delta Counters measures change since metric
-    was last recorded, and is prefixed with \u2206 (∆ - INCREMENT) or
-    \u0394 (Δ - GREEK CAPITAL LETTER DELTA)."""
+    """Wavefront SDK Delta Counter."""
