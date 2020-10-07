60b25deb3d3b45d323ada400558c45e421a33ffc
Joanna Ko <joannak@vmware.com>
2020-10-07 14:50:10 -0700

    Build Failure Fix: Delta Counter remove pass

--- wavefront_sdk/common/metrics/deltacounter.py
+++ wavefront_sdk/common/metrics/deltacounter.py
@@ -10,4 +10,3 @@ class WavefrontSdkDeltaCounter(WavefrontSdkCounter):
     """Wavefront SDK Delta Counter. Delta Counters measures change since metric
     was last recorded, and is prefixed with \u2206 (∆ - INCREMENT) or
     \u0394 (Δ - GREEK CAPITAL LETTER DELTA)."""
-    pass
