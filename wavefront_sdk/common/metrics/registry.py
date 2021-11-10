c61b2e72b9b4be5481085de599b8ac36f5c6eafb
Michaël Krens <mkrens@gmail.com>
2021-11-10 10:52:10 +0000

    Fix metrics being mutated during report

--- wavefront_sdk/common/metrics/registry.py
+++ wavefront_sdk/common/metrics/registry.py
@@ -41,7 +41,7 @@ class WavefrontSdkMetricsRegistry(object):
     # pylint: disable=broad-except
     def _report(self, timeout_secs=None):
         timestamp = time.time()
-        for key, val in self.metrics.items():
+        for key, val in self.metrics.copy().items():
             if timeout_secs and time.time() - timestamp > timeout_secs:
                 break
             name = self.prefix + key
