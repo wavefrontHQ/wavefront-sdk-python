64ffc3fbccfa7d0b3c59b51972e0705f9267df0b
Artem Ustinov <ustinov@users.noreply.github.com>
2022-09-08 17:27:01 -0700

    Restore PR Condition in Version Check. (#98)
    And few updates to README and setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
 
 setuptools.setup(
     name='wavefront-sdk-python',
-    version='1.8.9',  # The version number. Update with each pull request.
+    version='1.8.10',  # The version number. Update with each pull request.
     author='Wavefront by VMware',
     url='https://github.com/wavefrontHQ/wavefront-sdk-python',
     license='Apache-2.0',
@@ -27,10 +27,10 @@ setuptools.setup(
         ],
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         ],
     include_package_data=True,
     packages=setuptools.find_packages(exclude=('*.tests', '*.tests.*',
