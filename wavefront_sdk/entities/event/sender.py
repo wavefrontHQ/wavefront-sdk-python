7bd2e667f304d25c4e418d5d82933eb24104d4f4
Joanna Ko <joanna.ko@broadcom.com>
2025-07-15 15:04:06 -0700

    fix for jenkins jobs

--- wavefront_sdk/entities/event/sender.py
+++ wavefront_sdk/entities/event/sender.py
@@ -8,7 +8,7 @@
 class WavefrontEventSender(object):
     """Interface of Event Sender for both clients."""
 
-    # pylint: disable=too-many-arguments
+    # pylint: disable=too-many-arguments, too-many-positional-arguments
     def send_event(self, name, start_time, end_time, source,
                    tags, annotations):
         """Send Event Data.
